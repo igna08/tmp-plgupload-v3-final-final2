1cf058815046199d785e03ca21d381b8790b96e9
igna08 <156800096+igna08@users.noreply.github.com>
2025-06-28 13:19:23 -0300

    Update page.tsx

--- my-app/src/app/users/page.tsx
+++ my-app/src/app/users/page.tsx
@@ -27,7 +27,7 @@ import {
   Loader2
 } from 'lucide-react';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://finalqr-1-2-27-6-25.onrender.com/api';
 
 interface User {
   id: string;
