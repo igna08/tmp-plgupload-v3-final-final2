4bd60f6dc264bed88a7f4fd301c5fe58d73a61f5
igna08 <ignaciolibutti@gmail.com>
2025-10-23 17:31:06 -0300

    234

--- my-app/src/app/assets/page.tsx
+++ my-app/src/app/assets/page.tsx
@@ -943,7 +943,7 @@ const AllAssetsAdminPage: React.FC = () => {
                   Cerrar
                 </button>
                 <Link
-                  href={`/assets/${selectedAsset.id}/edit`}
+                  href={`/assets/${selectedAsset.id}`}
                   className="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
                 >
                   Editar Activo
